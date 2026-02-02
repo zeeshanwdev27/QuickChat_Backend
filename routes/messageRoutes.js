76d9f6412c7aeaab9a85ac35d035f877fef1574d
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2026-02-02 23:26:24 +0500

    final commit

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -7,7 +7,7 @@ const messageRouter = express.Router()
 
 messageRouter.get('/users', protectRoute, getUsersForSidebar)
 messageRouter.get('/:id', protectRoute, getMessages)
-messageRouter.put('mark/:id', protectRoute, markMessageAsSeen)
+messageRouter.put('/mark/:id', protectRoute, markMessageAsSeen)
 messageRouter.post('/send/:id', protectRoute, sendMessage)
 
 
